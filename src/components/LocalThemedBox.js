425ee6b8aeacbdceb9e87ee590ce9b9ad1653fbc
Chetan <mr.chetan005@gmail.com>
2023-08-28 02:37:22 +0530

    updated LocalThemedBox

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -15,7 +15,7 @@ const LocalThemedBox = () => {
         <div style={{ width: '200px', height: '200px', border: '2px solid green' }} id="local-themed-box" className={`bg-${localTheme}`}>
             {/* Write code below this line */}
             <p id='local-themed-text-container' className={`txt-${localTheme}`}></p>
-            <button onClick={() => setLocalTheme(prev => prev === 'dark' ? 'light' : 'dark')} id="local-theme-toggler" className={`btn btn-${localTheme}`} >Toggle local theme to {localTheme}</button>
+            <button onClick={() => setLocalTheme(prev => prev === 'dark' ? 'light' : 'dark')} id="local-theme-toggler" className={`btn btn-${localTheme}`} >Toggle local theme to {localTheme === 'dark' ? 'light' : 'dark'}</button>
         </div>
     )
 }
